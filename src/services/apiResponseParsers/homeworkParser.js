41755df900ca3573372ccabaa9f692ca4adac6ca
Валера <49568180+valerrkaaa@users.noreply.github.com>
2023-10-06 04:12:03 +0500

    getting home items list

--- src/services/apiResponseParsers/homeworkParser.js
+++ src/services/apiResponseParsers/homeworkParser.js
@@ -1,4 +1,7 @@
-import { createLessonApi } from "../apiRequests/homeworkRequests";
+import {
+    createLessonApi,
+    getLessonsApi,
+} from "../apiRequests/homeworkRequests";
 
 export const createLesson = async (token, model) => {
     let isSuccess = false;
@@ -8,6 +11,22 @@ export const createLesson = async (token, model) => {
             isSuccess = true;
             console.log("createLesson", response.data);
         })
-        .catch((errors) => {console.log(errors.response);});
-    return [isSuccess, content]
+        .catch((errors) => {
+            console.log(errors.response);
+        });
+    return [isSuccess, content];
+};
+
+export const getLessons = async (token) => {
+    let isSuccess = false;
+    let content = "";
+    await getLessonsApi(token)
+        .then((response) => {
+            isSuccess = true;
+            content = response.data.lessons;
+        })
+        .catch((errors) => {
+            console.log(errors.response);
+        });
+    return [isSuccess, content];
 };
