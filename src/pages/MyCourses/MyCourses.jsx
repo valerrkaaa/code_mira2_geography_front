1aad7b0d2a698334a4c59ff3d77de464d95346af
Artimald <59371589+Artimald@users.noreply.github.com>
2023-10-06 03:42:53 +0500

    nextStepDevelopment

--- src/pages/MyCourses/MyCourses.jsx
+++ src/pages/MyCourses/MyCourses.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./MyCourses.module.css";
 import Header from "../../components/header/Header";
 import SearchBar from "../../components/searchBar/SearchBar";
 import BlockCourses from "../../components/blockCourses/BlockCourses";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const MyCourses = () => {
   const navigate = useNavigate();
+
+  const [cookie] = useCookies(["role"]);
+  const [isTeacher, setIsTeacher] = useState(true);
+  useEffect(() => {
+    console.log(cookie.role);
+    setIsTeacher(cookie.role === "teacher");
+  }, [cookie]);
   return (
     <div className={classes.backGround}>
       <Header />
       <div className={classes.SearchLine}>
         <SearchBar placeholder="Поиск по названию курса" />
-        <Button
-          className={classes.btnAddCourses}
-          onClick={(e) => {
-            navigate("/createcourses");
-          }}
-        >
-          Добавить курс
-        </Button>
+        {isTeacher ? (
+          <Button
+            className={classes.btnAddCourses}
+            onClick={(e) => {
+              navigate("/createcourses");
+            }}
+          >
+            Добавить курс
+          </Button>
+        ) : (
+          <></>
+        )}
       </div>
       <BlockCourses />
     </div>
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -9,92 +9,92 @@ import { useCookies } from "react-cookie";
 import { authApi } from "../../services/apiRequests/AuthAPI";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const [model, setModel] = useState({
-        email: "",
-        password: "",
-    });
-    const [errors, setErrors] = useState();
-    const [isLoading, setLoading] = useState(false);
-    const [cookie, setCookie] = useCookies(["jwt"]);
+  const navigate = useNavigate();
+  const [model, setModel] = useState({
+    email: "",
+    password: ""
+  });
+  const [errors, setErrors] = useState();
+  const [isLoading, setLoading] = useState(false);
+  const [cookie, setCookie] = useCookies(["jwt"]);
 
-    const changeField = (field, value) => {
-        setModel({ ...model, [field]: value });
-    };
+  const changeField = (field, value) => {
+    setModel({ ...model, [field]: value });
+  };
 
-    const submit = (e) => {
-        e.preventDefault();
-        setErrors("");
+  const submit = (e) => {
+    e.preventDefault();
+    setErrors("");
 
-        setLoading(true);
-        (async () => {
-            authApi(model)
-                .then((response) => {
-                    setCookie("jwt", response.data.body.authorization.token);
-                    setCookie("role_id", response.data.body.role);
-                    window.location = `${process.env.REACT_APP_LOCAL_URL}/home`; // redirect to main page
-                })
-                .catch((error) => {
-                    let errorStatus = error.response.data.status;
-                    if (errorStatus === "validator error") {
-                        // массив ошибок
-                        let errors = Object.entries(error.response.data.errors);
-                        let oneError = errors[0][1][0];
+    setLoading(true);
+    (async () => {
+      authApi(model)
+        .then((response) => {
+          setCookie("jwt", response.data.body.authorization.token);
+          setCookie("role", response.data.body.role);
+          window.location = `${process.env.REACT_APP_LOCAL_URL}/home`; // redirect to main page
+        })
+        .catch((error) => {
+          let errorStatus = error.response.data.status;
+          if (errorStatus === "validator error") {
+            // массив ошибок
+            let errors = Object.entries(error.response.data.errors);
+            let oneError = errors[0][1][0];
 
-                        setErrors(oneError);
-                        console.log(oneError);
-                    } else if (errorStatus === "error") {
-                        setErrors(error.response.data.message);
-                        console.log(error.response.data.message);
-                    }
-                });
-        })();
-        setLoading(false);
-    };
+            setErrors(oneError);
+            console.log(oneError);
+          } else if (errorStatus === "error") {
+            setErrors(error.response.data.message);
+            console.log(error.response.data.message);
+          }
+        });
+    })();
+    setLoading(false);
+  };
 
-    return (
-        <>
-            <div className={classes.container}>
-                <form className={classes.blockInformation} onSubmit={submit}>
-                    <h2 className={classes.loginTitle}>Авторизация</h2>
-                    <DefaultInput
-                        placeholder="Логин"
-                        onChange={(e) => {
-                            changeField("email", e.target.value);
-                        }}
-                        value={model.email}
-                    ></DefaultInput>
-                    <DefaultInput
-                        placeholder="Пароль"
-                        onChange={(e) => {
-                            changeField("password", e.target.value);
-                        }}
-                        value={model.password}
-                        type="password"
-                    ></DefaultInput>
-                    <div className={classes.infAboutError}>{errors}</div>
-                    <Button
-                        disabled={isLoading}
-                        className={classes.btnLogin}
-                        type="submit"
-                    >
-                        {isLoading ? <>Загрузка...</> : <>Войти</>}
-                    </Button>
-                    <span className={classes.footerLogin}>
-                        Нет аккаунта?
-                        <span
-                            className={classes.registerLink}
-                            onClick={(e) => {
-                                navigate("/registration");
-                            }}
-                        >
-                            Зарегистрироваться
-                        </span>
-                    </span>
-                </form>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={classes.container}>
+        <form className={classes.blockInformation} onSubmit={submit}>
+          <h2 className={classes.loginTitle}>Авторизация</h2>
+          <DefaultInput
+            placeholder="Логин"
+            onChange={(e) => {
+              changeField("email", e.target.value);
+            }}
+            value={model.email}
+          ></DefaultInput>
+          <DefaultInput
+            placeholder="Пароль"
+            onChange={(e) => {
+              changeField("password", e.target.value);
+            }}
+            value={model.password}
+            type="password"
+          ></DefaultInput>
+          <div className={classes.infAboutError}>{errors}</div>
+          <Button
+            disabled={isLoading}
+            className={classes.btnLogin}
+            type="submit"
+          >
+            {isLoading ? <>Загрузка...</> : <>Войти</>}
+          </Button>
+          <span className={classes.footerLogin}>
+            Нет аккаунта?
+            <span
+              className={classes.registerLink}
+              onClick={(e) => {
+                navigate("/registration");
+              }}
+            >
+              Зарегистрироваться
+            </span>
+          </span>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Login;
