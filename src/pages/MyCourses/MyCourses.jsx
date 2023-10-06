b0e4f0b4149f3477374f0432ba60eeb35f5ae81b
Artimald <59371589+Artimald@users.noreply.github.com>
2023-10-06 09:11:05 +0500

    startJob

--- src/pages/MyCourses/MyCourses.jsx
+++ src/pages/MyCourses/MyCourses.jsx
@@ -9,45 +9,54 @@ import { useCookies } from "react-cookie";
 import { getTeacherLessons } from "../../services/apiResponseParsers/homeworkParser";
 
 const MyCourses = () => {
-    const navigate = useNavigate();
-    const [lessons, setLessons] = useState([]);
-    const [cookies] = useCookies();
+  const navigate = useNavigate();
+  const [lessons, setLessons] = useState([]);
+  const [cookies] = useCookies();
 
-    const [isTeacher, setIsTeacher] = useState(true);
-    useEffect(() => {
-        setIsTeacher(cookies.role === "teacher");
-        getTeacherLessons(cookies.jwt)
-            .then((response) => {
-                if (response[0]) {
-                    setLessons(response[1]);
-                }
-            })
-            .catch();
-    }, [cookies]);
+  const [isTeacher, setIsTeacher] = useState(true);
+  useEffect(() => {
+    setIsTeacher(cookies.role === "teacher");
+    getTeacherLessons(cookies.jwt)
+      .then((response) => {
+        if (response[0]) {
+          setLessons(response[1]);
+        }
+      })
+      .catch();
+  }, [cookies]);
 
-    return (
-        <div className={classes.backGround}>
-            <Header />
-            <div className={classes.SearchLine}>
-                <SearchBar placeholder="Поиск по названию курса" />
-                {isTeacher ? (
-                    <Button
-                        className={classes.btnAddCourses}
-                        onClick={(e) => {
-                            navigate("/createcourses");
-                        }}
-                    >
-                        Добавить курс
-                    </Button>
-                ) : (
-                    <></>
-                )}
-            </div>
-            {lessons.map((item) => {
-                return <BlockCourses key={item.id} id={item.id} name={item.name} photo={item.photo}/>;
-            })}
-        </div>
-    );
+  return (
+    <div className={classes.backGround}>
+      <Header />
+      <div className={classes.SearchLine}>
+        <SearchBar placeholder="Поиск по названию курса" />
+        {isTeacher ? (
+          <Button
+            className={classes.btnAddCourses}
+            onClick={(e) => {
+              navigate("/createcourses");
+            }}
+          >
+            Добавить курс
+          </Button>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className={classes.listTask}>
+        {lessons.map((item) => {
+          return (
+            <BlockCourses
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              photo={item.photo}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default MyCourses;
